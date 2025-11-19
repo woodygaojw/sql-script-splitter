2fe0ec7ddb08362ed11d174d761007d80ea20480
gjw <3134897@qq.com>
2025-11-19 20:40:34 +0800

    create unit test

--- src/test/java/com/example/SqlScriptSplitterTest.java
+++ src/test/java/com/example/SqlScriptSplitterTest.java
@@ -11,7 +11,7 @@ class SqlScriptSplitterTest {
 
     @Test
     void split() {
-        String script = "select * from table1; select * from table2;";
+        String script = "select * from table1 where a='1;'; select * from table2;";
         SqlScriptSplitter sqlScriptSplitter = new SqlScriptSplitter(script);
         List<String> sqls = sqlScriptSplitter.split();
         assertThat(sqls).hasSize(2);
